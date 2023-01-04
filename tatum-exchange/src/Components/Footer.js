0e119157f1bcc9c34eb70d4fe5c85106ff6da7b6
Allan mang'eni <allanwanyonyi@protonmail.com>
2023-01-04 13:20:31 +0000

    create footer

---
+++ tatum-exchange/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <p>Copyright © DEX. All rights reserved.</p>
+        <p>Terms of Service | Privacy Policy</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
